8d968de932bc808e73a94e3c7e21ea5b5174a5f7
webdev-ashishk <webdev.ashishk@gmail.com>
2023-08-29 11:05:50 +0530

    lecture 3 completd

--- src/app/page.js
+++ src/app/page.js
@@ -8,7 +8,9 @@ export default function Home() {
         <p>
           Get started by editing&nbsp;
           <code className={styles.code}>src/app/page.js</code>
+          <h2>Welcome to AwesomePlace</h2>
         </p>
+        
         <div>
           <a
             href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
